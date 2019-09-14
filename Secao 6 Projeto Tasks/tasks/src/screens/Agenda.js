a5d2dc3c8b163ab8de31d7ca3d12b07b3b04540e
Matheus Barbosa <matheus.barbosa@dce.ufpb.br>
2019-09-14 16:52:28 -0300

    Seção 6 - Aula 66

--- Secao 6 Projeto Tasks/tasks/src/screens/Agenda.js
+++ Secao 6 Projeto Tasks/tasks/src/screens/Agenda.js
@@ -1,2 +1,68 @@
-import React, {Component} from react
-import {View, Text, ImageBackground, StyleSheet} from react-native
+import React, { Component } from 'react';
+
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import todayImage from '../../assets/imgs/today.jpg'
+import commonStyle from '../commonStyle'
+import Task from '../components/Task'
+
+export default class Agenda extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>
+                            Hoje
+                        </Text>
+                        <Text style={styles.subTitle}>
+                            {moment().locale('pt-br').format('ddd, D [de] MMMM')}
+                        </Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskContainer}>
+                    <Task
+                        desc='Tarefa pendende'
+                        estimateAt={new Date()}
+                        doneAt={null} />
+                    <Task
+                        desc='Tarefa Concluída'
+                        estimateAt={new Date()}
+                        doneAt={new Date()} />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 3,
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: commonStyle.fontFamily,
+        color: commonStyle.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 10,
+    },
+    subTitle: {
+        fontFamily: commonStyle.fontFamily,
+        color: commonStyle.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30,
+    },
+    taskContainer: {
+        flex: 7,
+    }
+
+})
