3abd3673dbf197e7330335927c1e3020ae811a28
matheus.barbosa <matheus.barbosa@phoebus.local>
2019-09-23 14:15:30 -0300

    Seção 7 - Aula 84

---
+++ Secao 7 Projeto Tasks Backend/tasks-backend/api/task.js
@@ -0,0 +1,40 @@
+const moment = require('moment')
+
+module.exports = app => {
+    const getTasks = (req, res) => {
+        const date = req.query.date ? req.query.date : moment().endOf('day').toDate()
+
+        app.db('tasks')
+            .where({ userId: req.user.id })
+            .where('estimateAt', '<=', date)
+            .orderBy('estimateAt')
+            .then(tasks => res.json(tasks))
+            .catch(err => res.status(500).json(err))
+    }
+
+    const save = (req, res) => {
+        if (!req.body.desc.trim()) {
+            return res.status(400).send("Descrição é obrigatorio.")
+        }
+        req.body.userId = req.user.id
+
+        app.db('tasks')
+            .insert(req.body)
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(500).json(err))
+    }
+
+    const remove = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .delete()
+            .then(rowsDeleted => {
+                if (rowsDeleted > 0) {
+                    res.status(204).send()
+                } else {
+                    res.status(404).send(`Nenhuma task encontrada com id: ${req.params.id}.`)
+                }
+            })
+            .catch(err => res.status(500).json(err))
+    }
+}
