1cad436588c4dddda678a3f23b443b6a31672b92
Matheus Barbosa <matheus.barbosa@dce.ufpb.br>
2019-09-29 16:06:54 -0300

    Seção 8 - Aula 92

--- Secao 6 Projeto Tasks/tasks/src/screens/Auth.js
+++ Secao 6 Projeto Tasks/tasks/src/screens/Auth.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
-    TextInput,
     View,
     ImageBackground,
     TouchableOpacity,
     Alert
 } from 'react-native';
+import axios from 'axios';
+import { server, showError } from '../common'
 import commonStyle from '../commonStyle';
 import backgoundImage from '../../assets/imgs/login.jpg';
-import AuthInput  from '../components/AuthInput';
+import AuthInput from '../components/AuthInput';
 
 
 export default class Auth extends Component {
@@ -29,11 +30,33 @@ export default class Auth extends Component {
      * @param: none
      * @returns: none
      */
-    signinOrSignup = () => {
+    signinOrSignup = async () => {
         if (this.state.stageNew) {
-            Alert.alert('Sucesso!', 'Criar conta');
+            try {
+                await axios.post(`${server}/signup`, {
+                    name: this.state.name,
+                    email: this.state.email,
+                    password: this.state.password,
+                    confirmPassword: this.state.confirmPassword,
+                });
+                Alert.alert('Sucesso!', 'Usuario cadastrado.');
+                this.setState({ stageNew: false });
+            } catch (err) {
+                showError(err);
+            };
         } else {
-            Alert.alert('Sucesso!', 'Logar');
+            try {
+                const res = await axios.post(`${server}/signin`, {
+                    email: this.state.email,
+                    password: this.state.password,
+                });
+                axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`;
+                this.props.navigation.navigate('Home');
+
+            } catch (err) {
+                Alert.alert('Erro', 'Falha no login')
+                //showError(err);
+            };
         }
     };
 
